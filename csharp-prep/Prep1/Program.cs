d361aa38d0e4fb1150d3045804104cd12941855f
camilleechip <camilleechip@gmail.com>
2025-09-16 13:38:16 -0600

    Prep 1 code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your name?");
+        string fname = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
+    
