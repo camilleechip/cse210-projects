6fc786f863d67019864f2fb7bee7809269aa37c8
camilleechip <camilleechip@gmail.com>
2025-09-23 13:19:56 -0600

    Prep3 Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 100);
+
+        int number;
+
+        do
+        {
+            Console.Write("What is your guess?");
+            string guess = Console.ReadLine();
+            number = int.Parse(guess);
+
+            if (number == magicNum)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (number > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (number < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (number != magicNum);
     }
+    
+
+
 }
