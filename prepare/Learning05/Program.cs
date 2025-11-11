60db30dbdd436be28fa1794d85094ce2ed1db4db
Camille Huggins <camilleechip@gmail.com>
2025-11-11 13:44:49 -0700

    learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Drawing;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Pink", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Green", 3, 4);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Blue", 3);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
